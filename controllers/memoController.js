98d4117b248be94c3a3dd705535e95886dc88fa1
Diky Arga <dikyarga.id@gmail.com>
2017-03-23 22:49:27 +0800

    Update and delete memo done

--- controllers/memoController.js
+++ controllers/memoController.js
@@ -17,5 +17,40 @@ module.exports = {
         msg: 'Success submit an memo'
       })
     })
+  },
+  update: function(req, res, next){
+    db.Memo.update({
+      title: req.body.title,
+      content: req.body.content,
+      completed: req.body.completed
+    }, {
+      where: {
+        id: req.params.id
+      }
+    }).then((result) => {
+      if (!result) {
+        res.json({
+          success: false,
+          msg: 'fail when trying to update the data'
+        })
+      }
+
+      res.json({
+        success: true,
+        msg: 'Data updated.'
+      })
+    })
+  },
+  destroy: function(req, res, next){
+    db.Memo.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then((deletedQuestion) => {
+      res.json({
+        success: true,
+        msg: 'Question deleted'
+      })
+    })
   }
 }
--- routes/memos.js
+++ routes/memos.js
@@ -7,4 +7,8 @@ router.get('/', memoController.index);
 
 router.post('/', memoController.create);
 
+router.put('/:id', memoController.update);
+
+router.delete('/:id', memoController.destroy);
+
 module.exports = router;
